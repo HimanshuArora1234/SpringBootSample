ca4f5f90c778dfb78c87432e20275249c8897bd7
himanshuarora <himanshu.arora.alw@gmail.com>
2016-08-02 12:32:21 +0200

    project commit

---
+++ src/main/java/hello/SampleController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index.html";
+    }
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+    public String home1() {
+        return "hello !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
