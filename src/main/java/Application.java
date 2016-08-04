9f41ce12a16fd736a2424053c8eb457133495f27
guillaume gallois <guillaume.gallois@gmail.com>
2016-08-04 10:10:44 +0200

    test swagger

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,22 @@
-
-
-
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
 
 @Configuration
 @EnableAutoConfiguration
@@ -15,10 +24,30 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EntityScan(basePackages = "entities")
 @EnableJpaRepositories(basePackages = "repositories")
+@EnableSwagger2
 @ComponentScan(basePackages = { "services", "controllers" })
 public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    // see http://heidloff.net/article/usage-of-swagger-2-0-in-spring-boot-applications-to-document-apis/
+    // for Swagger test
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .build();
     }
 }
--- src/main/java/controllers/DaoController.java
+++ src/main/java/controllers/DaoController.java
@@ -1,46 +1,57 @@
 package controllers;
 
-import java.util.List;
-
+import entities.User;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import entities.User;
 import services.UserService;
+
+import java.util.List;
 
 @Controller
 public class DaoController {
-	@Autowired
-	private UserService userService;
-	
-    @RequestMapping("/")
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET, path="/")
     public String home() {
         return "index.html";
     }
-	
-    @RequestMapping("/hello")
+
+    @RequestMapping(method = RequestMethod.GET, path="/hello")
     @ResponseBody
     public String home1() {
         return "hello !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     }
-    
-    @RequestMapping("/add")
+
+    @RequestMapping(method = RequestMethod.POST, path="/add")
     @ResponseBody
     @Transactional(readOnly = true)
     public User addUser() {
-	User user = new User();
-	user.setName("steve");
-	user.setEmail("steve@gmail.com");
-	return this.userService.create(user);
+        User user = new User();
+        user.setName("steve");
+        user.setEmail("steve@gmail.com");
+        return this.userService.create(user);
     }
+
+    @ApiOperation(value = "getAll", nickname = "getAll")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = User.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
     @GetMapping("/all")
     @ResponseBody
     @Transactional(readOnly = true)
     public List<User> getAll() {
-	return this.userService.findAll();
+        return this.userService.findAll();
     }
 }
